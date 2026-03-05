2ec7bbaab3d6e714c569261be57297ddae892153
ishayat <ishayataronno.13@gmail.com>
2026-03-05 13:07:33 +0600

    oms_service functional up to crud operations using db

---
+++ auth_service/go.mod
@@ -0,0 +1,3 @@
+module orderly/auth-service
+
+go 1.25.6
--- oms_service/cmd/oms/main.go
+++ oms_service/cmd/oms/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"orderly/oms-service/internal/database"
 	"orderly/oms-service/internal/handlers"
+	"orderly/oms-service/internal/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,10 +15,9 @@ func main() {
 	router := gin.Default()
 	router.GET("/orders", handlers.GetOrders)
 	router.POST("/orders", handlers.CreateOrder)
-	router.GET("/orders/:id", handlers.ValidateID(), handlers.GetOrderByID)
-	router.DELETE("/orders/:id", handlers.ValidateID(), handlers.DeleteOrder)
-	router.PUT("/orders/:id",handlers.ValidateID(), handlers.UpdateOrder)
-
+	router.GET("/orders/:id", middlewares.ValidateID(), handlers.GetOrderByID)
+	router.DELETE("/orders/:id", middlewares.ValidateID(), handlers.DeleteOrder)
+	router.PUT("/orders/:id",middlewares.ValidateID(), handlers.UpdateOrder)
 
 	router.Run("localhost:8080")
 }
